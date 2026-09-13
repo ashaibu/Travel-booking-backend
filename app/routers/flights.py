197f1000231ba236aab65de6bb7ed416144c914f
ashaibu <asean3033@gmail.com>
2026-09-13 11:51:01 +0100

    my continuation

--- app/routers/flights.py
+++ app/routers/flights.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from app.schemas.flight import FlightResponse, FlightCreate 
 
 router = APIRouter()
 
@@ -27,8 +28,29 @@ flights = [
 ]
 
 
-@router.get("/flights")
-def get_flights():
-    return {
-        "flights": flights
+@router.get("/flights/{flight_id}", response_model=FlightResponse)
+def get_flight(flight_id: int):
+
+    for flight in flights: 
+        if flight["id"] == flight_id: 
+            return flight
+
+    raise HTTPException(status_code=404, detail="flight not found")
+
+
+@router.post("/flights", response_model=FlightResponse)
+def create_flight(flight: FlightCreate):
+    new_flight = {
+        "id": len(flights) + 1,
+        "airline": flight.airline,
+        "from": flight.from_,
+        "to": flight.to,
+        "departure": flight.departure,
+        "arrival": flight.arrival,
+        "price": flight.price,
+        "available_seats": flight.available_seats
     }
+
+    flights.append(new_flight)
+
+    return new_flight
---
+++ app/schemas/flight.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field 
+
+
+class FlightResponse(BaseModel):
+    id:int 
+    airline: str 
+    from_: str = Field(alias="from")
+    to: str 
+    departure: str 
+    arrival: str
+    price: int 
+    available_seats: int 
+
+    class Config: 
+        populate_by_name = True
+
+
+class FlightCreate(BaseModel):
+    airline: str
+    from_: str = Field(alias="from")
+    to: str
+    departure: str
+    arrival: str
+    price: int
+    available_seats: int
+
+    class Config:
+        populate_by_name = True
